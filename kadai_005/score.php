c01151ce2acf138ab71b8188d2f42b35f4d62b54
minog7330 <135280425+minog7330@users.noreply.github.com>
2023-06-03 08:49:37 +0900

    score.php 変更1

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -10,5 +10,12 @@ $score8 = 95;
 $score9 = 30;
 $score10 = 60;
 
-echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
+// 変数$total_scoreに10人分のテストの合計点を代入
+$total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+// 変数$average_scoreにテストの平均点を代入
+$average_score = $total_score / 10;
+
+// 変数$avarage_scoreを画面に出力
+echo $avarage_score;
 ?>
